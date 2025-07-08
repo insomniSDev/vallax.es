ed494d029acb53cc0f794188bb832473c218799b
insomnis <insomnisdev@gmail.com>
2025-07-08 14:33:31 +0200

    github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://vallax.es',
   integrations: [
     starlight({
       title: "Vallax Docs",
