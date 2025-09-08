8ac1900db65f50d2f2605bb9ec869a4333a9ddba
jaime <jaime@gmail.com>
2025-09-08 09:51:23 +0200

    [UPDATE] images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://vallax.es',
   base: '/',
   vite: {
     plugins: [tailwindcss()],
+    
   },
    prefetch: {
     prefetchAll: true
