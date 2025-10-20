107c338761ddb26b9a75c4deef5f332f3a7a4da3
insomnis <insomnisdev@gmail.com>
2025-10-20 13:40:33 +0200

    remove netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import netlify from "@astrojs/netlify";
 import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
