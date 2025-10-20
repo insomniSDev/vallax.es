b0d09808838abf1df92a3ad97f0e6df35c8d7249
jaime <jaime@gmail.com>
2025-10-20 15:33:20 +0200

    [UPDATE] remove react impots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://vallax.es",
@@ -23,5 +22,5 @@ export default defineConfig({
     },
   },
 
-  integrations: [react()],
+  integrations: [],
 });
